7bf7c05bece7c19ed65b83c4f20ba68c910679d2
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2026-06-04 03:47:28 +0330

    fix: add missing app.config.ts

---
+++ mobile/app.config.ts
@@ -0,0 +1,10 @@
+export default {
+    expo: {
+        android: {
+            package: "ir.hirbodcodes.memoize"
+        },
+        ios: {
+            bundleIdentifier: "ir.hirbodcodes.memoize"
+        }
+    }
+};
