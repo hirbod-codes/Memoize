25ca6cd8f7b2534ad9d6b94d37284fcd149165b3
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2026-06-08 22:56:01 +0330

    fix: release.yml

--- client/update_flutter_version.dart
+++ client/update_flutter_version.dart
@@ -1,9 +1,13 @@
 // tool/update_version.dart
+import 'dart:developer';
 import 'dart:io';
 
 void main(List<String> args) {
   final version = args[0];
   final build = args[1];
+
+  log('version: $version');
+  log('build: $build');
 
   final file = File('pubspec.yaml');
   final content = file.readAsStringSync();
