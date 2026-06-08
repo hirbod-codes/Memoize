4863fe992a0b8b700e38615b0e28a6387ca19dec
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2026-06-08 23:26:44 +0330

    fix: release.yml

--- client/update_flutter_version.dart
+++ client/update_flutter_version.dart
@@ -1,12 +1,15 @@
-import 'dart:developer';
 import 'dart:io';
 
 void main(List<String> args) {
-  final version = args[0];
-  final build = args[1];
+  final version = Platform.environment['VERSION'];
+  final build = Platform.environment['BUILD_NUMBER'];
 
-  log('version: $version');
-  log('build: $build');
+  if (version == null || build == null) {
+    throw Exception('VERSION or BUILD_NUMBER missing');
+  }
+
+  print('version: $version');
+  print('build: $build');
 
   final file = File('pubspec.yaml');
   final content = file.readAsStringSync();
