b979ec70ef1463441948b2f1bc8bf8d41166ca6c
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2026-06-22 08:17:58 +0330

    add s3 bucket storage support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -37,6 +37,8 @@ import { authRoutes } from './routes/auth';
 import { userRoutes } from './routes/user';
 import { ttsRoutes } from './routes/tts';
 
+import { S3Client } from "@aws-sdk/client-s3";
+
 dotenv.config({ debug: process.env.DEBUG !== undefined ? Boolean(process.env.DEBUG) : undefined })
 
 export const isProduction = getStringEnv('NODE_ENV', 'The Node env environment variable is not provided')! === 'production'
@@ -49,9 +51,13 @@ export const accessTokenSecret = getStringEnv('ACCESS_TOKEN_SECRET', 'The access
 export const refreshTokenSecret = getStringEnv('REFRESH_TOKEN_SECRET', 'The refresh token secret environment variable is not provided')
 export const allowedOrigins = getStringEnv('ALLOWED_ORIGINS', 'The Allowed origins environment variable is not provided')!
 
-export const meilisearchKey = getStringEnv('MEILISEARCH_KEY', 'The Allowed origins environment variable is not provided')!
-export const meilisearchHost = getStringEnv('MEILISEARCH_HOST', 'The Allowed origins environment variable is not provided')!
-export const meilisearchPort = getIntegerEnv('MEILISEARCH_PORT', 'The Allowed origins environment variable is not provided')!
+export const meilisearchKey = getStringEnv('MEILISEARCH_KEY', 'The Meilisearch key environment variable is not provided')!
+export const meilisearchHost = getStringEnv('MEILISEARCH_HOST', 'The Meilisearch host environment variable is not provided')!
+export const meilisearchPort = getIntegerEnv('MEILISEARCH_PORT', 'The Meilisearch port environment variable is not provided')!
+
+export const s3Endpoint = getStringEnv('S3_STORAGE_ENDPOINT', 'The S3 storage endpoint environment variable is not provided')!
+export const s3AccessKey = getStringEnv('S3_STORAGE_ACCESS_KEY', 'The S3 storage access key environment variable is not provided')!
+export const s3SecretKey = getStringEnv('S3_STORAGE_SECRET_KEY', 'The S3 storage secret key environment variable is not provided')!
 
 export const dbConfig = {
     databaseName: getStringEnv('DB_DATABASE_NAME', 'The Db database name environment variable is not provided'),
@@ -66,6 +72,16 @@ export const dbConfig = {
 export const meili = new Meilisearch({
     host: meilisearchHost + ':' + meilisearchPort.toString(),
     apiKey: meilisearchKey
+});
+
+export const s3 = new S3Client({
+    followRegionRedirects: true,
+    endpoint: s3Endpoint,
+    credentials: {
+        accessKeyId: s3AccessKey,
+        secretAccessKey: s3SecretKey
+    },
+    forcePathStyle: true // often required for S3-compatible services
 });
 
 (async () => {
